5e0b64d0645db5f77850c6c292a387e4d3b1b8e1
jjmmnd <rnastery1@gmail.com>
2025-03-27 19:20:11 +0900

    Initial Commit!

---
+++ src/main/java/com/likelion/practice0/Practice0Application.java
@@ -0,0 +1,13 @@
+package com.likelion.practice0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practice0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practice0Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/likelion/practice0/Practice0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.practice0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practice0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
